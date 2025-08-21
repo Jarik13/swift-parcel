402dfb1fa0d0b1649479f887c7c4479b303ffc0b
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-21 19:28:24 +0300

    Create auth controller

---
+++ auth-service/src/main/java/org/example/authservice/controller/AuthController.java
@@ -0,0 +1,39 @@
+package org.example.authservice.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.authservice.dto.auth.LoginRequest;
+import org.example.authservice.dto.auth.RegistrationRequest;
+import org.example.authservice.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegistrationRequest request, HttpServletResponse response) {
+        authService.register(request, response);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response) {
+        authService.login(request, response);
+        return ResponseEntity.ok("Login successful");
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<String> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        authService.refreshToken(request, response);
+        return ResponseEntity.ok("Access token refreshed");
+    }
+}
